e51e641f03eb03956e4497bb7e74b79841eae911
ssleepy00 <115939516+ssleepy00@users.noreply.github.com>
2025-04-09 01:55:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#dataset an array of coordinates, so [(0,2),(3,5)] predicted follows a similar format, but takes x coordinate from dataset and uses model to find predicted y val
+def MSE(dataset,predicted):
+    assert len(dataset) == len(predicted)
+    total=0
+    for i in range(len(dataset)):
+        total = total + ((dataset[i][1] - predicted[i][2])**2) 
+    return total * 1/len(dataset)
