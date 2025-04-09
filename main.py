f8f6b47d919a7a92ea29714c0d1513d6b11c9ad8
ssleepy00 <115939516+ssleepy00@users.noreply.github.com>
2025-04-09 16:28:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #dataset an array of coordinates, so [(0,2),(3,5)] predicted follows a similar format, but takes x coordinate from dataset and uses model to find predicted y val
 def MSE(dataset,predicted):
     assert len(dataset) == len(predicted)
+    assert type(dataset) == "list"
+    assert type(predicted) == "list"
     total=0
     for i in range(len(dataset)):
         total = total + ((dataset[i][1] - predicted[i][2])**2) 
+    return total * 1/len(dataset)
+def MAE(dataset,predicted):
+    assert len(dataset) == len(predicted)
+    assert type(dataset) == "list"
+    assert type(predicted) == "list"
+    total=0
+    for i in range(len(dataset)):
+        total= total + abs(dataset[i][1] - predicted[i][1])
     return total * 1/len(dataset)
